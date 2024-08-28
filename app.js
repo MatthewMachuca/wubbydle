7ce9b28abbe4fa28a383d96649e7ba57589d4568
unknown <matthewmachuca457@gmail.com>
2024-08-27 20:18:02 -0700

    new bird

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let buttonTarget = document.querySelector('button');
 
 function ping() {
-	document.getElementById("all").innerHTML = "<img src= 'bird.jpeg' width='1000px' height='1000px' display= 'flex' justify-content = 'center'  >  "
+	document.getElementById("all").innerHTML = "<img src= 'newbird.jpeg' width='1000px' height='1000px' display= 'flex' justify-content = 'center'  >  "
 }
 
 buttonTarget.addEventListener('click', ping);
